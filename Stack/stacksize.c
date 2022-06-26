396cdd34e0daf58ad0336cd5ea1759bd85b97365
dxlearn <dx_learn@163.com>
2022-06-26 22:01:20 +0800

    add int 0x80

--- Stack/stacksize.c
+++ Stack/stacksize.c
@@ -8,8 +8,8 @@ void blow_stack() {
 }
  
 int main() {
-    register void *pvEbp __asm__ ("%esp");
-    orig_stack_pointer = pvEbp;
+    __asm__("movq %rsp,orig_stack_pointer");
     blow_stack();
     return 0;
 }
+//编译时要加-no-pie字段 https://www.cnblogs.com/sky-heaven/p/15953338.html
